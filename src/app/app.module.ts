0c5c660da9dd9e294e728f1839e8cec0420707a6
Mariel <marielk.mya@gmail.com>
2018-09-07 12:04:08 -0300

    creando componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WallComponent } from './wall/wall.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    WallComponent
   ],
   imports: [
     BrowserModule
