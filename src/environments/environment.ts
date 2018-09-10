a15c4808f80ab05d126df72c8695e61a650c7674
Karyn Herrera <karynherrera@gmail.com>
2018-09-10 09:41:06 -0300

    añadiendo dependencias de angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'XXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXX',
+    databaseURL: 'XXXXXXXXXXX',
+    projectId: 'XXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXX',
+    messagingSenderId: 'XXXXXXXXXXX'
+  }
 };
-
 /*
  * In development mode, for easier debugging, you can ignore zone related error
  * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
