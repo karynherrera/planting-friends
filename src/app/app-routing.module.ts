e9ca06aa429157e1e6e0eeee59a06865516ef333
Mariel <marielk.mya@gmail.com>
2018-09-12 12:45:04 -0300

    creando rutas con modulo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule, Component } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WallComponent } from './wall/wall.component';
+
+const app_routes: Routes=[
+  {
+    path: '', component: WelcomeComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'wall', component: WallComponent
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: ''
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(app_routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+
+}
