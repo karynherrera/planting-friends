d0afa62f714f999cd2520b72e93236d9e9d2a08e
Karyn Herrera <karynherrera@gmail.com>
2018-09-24 00:48:06 -0300

    se unifican cambios

--- src/app/wall/publicaciones/publicaciones.component.ts
+++ src/app/wall/publicaciones/publicaciones.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { PublishInterface } from '../../models/publishInterface';
 import { PublicacionesService } from '../../services/publicaciones.service';
 
+
 @Component({
   selector: 'app-publicaciones',
   templateUrl: './publicaciones.component.html',
   styleUrls: ['./publicaciones.component.css']
 })
 export class PublicacionesComponent implements OnInit {
+
 
   constructor(private pubService: PublicacionesService) { }
 
