a4448e5d9c1d95e00976bc2a3f2202d9e2a5453d
Mariel <marielk.mya@gmail.com>
2018-09-27 17:32:54 -0300

    subir fotos al post ok

--- src/app/wall/add-publish/add-publish.component.ts
+++ src/app/wall/add-publish/add-publish.component.ts
@@ -72,7 +72,6 @@ export class AddPublishComponent implements OnInit {
         this.publicacion.date = date;
         this.publicacion.name = name;
         this.publicacion.photoUrl = photo;
-        this.publicacion.imgPublish= this.urlColection;
         //this.pubServicio.addPublish(this.publicacion);
         this.pubServicio.addPublish(this.publicacion);
       } 
@@ -90,109 +89,25 @@ export class AddPublishComponent implements OnInit {
     // subir archivo a la base de datos
     const ref = this.storage.ref(fileroot);
     const task = this.storage.upload(fileroot, this.selectedFile);
-    // const task = this.storage.upload(fileroot, this.selectedFile);
+
     // esperar a que este cargado en la base de datos
     task.snapshotChanges().pipe(
         finalize(() => {
            // obtener url correcta para subir a la coleccion 
      this.profileUrl = ref.getDownloadURL();
      this.profileUrl.subscribe(url=>{
-       // crear imagen en la memoria para dibujarla en canvas
+       // crear imagen en la memoria para dibujarla 
        const myImage = new Image();
        myImage.src = url;
-       // console.log('myImg' + myImage);
-       const imgPlace = document.getElementById('place_img');
-       this.urlColection = myImage.src;
-       // esta es la url que sirve
-      // const url64 = this.getBase64Image(myImage); 
-      // console.log('url64'+ url64);
+       // pasarle el valor al post. 
+       this.publicacion.imgPublish = myImage.src;
       
           })
         })
     ).subscribe()
  
-  //   this.uploadPercent= task.percentageChanges();
-  //   task.snapshotChanges().pipe(
-  //     finalize(() => {
-  //        this.downloadURL = ref.getDownloadURL()
-  //        this.downloadURL.subscribe(url=>{
-  //         const gsUrl = firebase.storage().refFromURL(url).toString();
-  //         console.log(gsUrl);
-  //         this.urlColection = gsUrl;
-  //        })
-  //       })
-  //  )
-  // .subscribe()
-  //   //const donwloadURL = task.downloadURL();
-  //   //console.log(this.selectedFile.name);root
-    
   }
 
-  // getBase64Image(img) {
-  //   const canvas =  <HTMLCanvasElement>this.myCanvas.nativeElement;
-  //   this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
-  //   this.context.drawImage(img, 0, 0);
-  //   var dataURL = canvas.toDataURL();
-  //   return dataURL;
-  // }
-  
-
-//   const gsUrl = firebase.storage().refFromURL(url).toString();
-//   // pasar la url a la coleccion 
-//   // Create a child reference
-//   const imagesRef = ref.child(fileroot);
-//   // subir esta url 
-//  // pasarle la url directo 
- 
-//  // this.urlColection = JSON.stringify(url);
-//  console.log(JSON.stringify(gsUrl));
- 
-//  const message = JSON.stringify(gsUrl);
-//  imagesRef.putString(message).then(function(snapshot) {
-//      console.log('Uploaded a raw string!');
-//    });
-//  })
-  
-  // getUrl(filePath){
-  //   let urlImg;
-  //    const ref = this.storage.ref(filePath);
-  //    this.profileUrl = ref.getDownloadURL();
-  //    this.profileUrl.subscribe(url=>{
-  //      urlImg=url
-  //      console.log(urlImg);
-  //      const gsUrl = firebase.storage().refFromURL(urlImg).toString();
-  //      console.log(gsUrl);
-  //      this.urlColection = gsUrl;
-  //     })
-      
-     //console.log(ref);
-    //let stRef = firebase.storage().ref();
-    //let imgRef = stRef.child('imgs');
-    //let path = 'imgs/'+filePath;
-    //console.log(path);
-    /*
-    storage.child(filePath).getDownloadURL().then(function(url) {
-      urlImg = url;
-      console.log(url);
-      const ref = this.storage.ref(`${filePath}`);
-      this.profileUrl = ref.getDownloadURL();
-      console.log(ref.getDownloadURL());
-       //return console.log(urlImg);
-       //return urlImg;
-    }).catch(function(error) {
-      console.log(error);
-      // Handle any errors
-    });
-    */
-    //console.log(imagesRef);
-    //let imagenUrl= this.storage.ref(filePath).getDownloadURL;
-    //console.log(imagenUrl);
-    //const ref = this.storage.ref(filePath);
-    //const task2 = ref.put(this.selectedFile);
-    //console.log(task2);
-    
-    //const downloadUrl = "https://firestorage.googleapis...";
-    //const gsUrl = firebase.storage().refFromUrl(downloadUrl).toString();
   }
   
   
